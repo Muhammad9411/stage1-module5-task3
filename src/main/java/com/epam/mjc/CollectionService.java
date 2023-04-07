0fc50aeec4e482374e0d8785dded392ba9d99fed
MuhammadJon <mtjohn9411@gmail.com>
2023-04-07 11:06:41 +0500

    Stage 1. Stream API task has been developed

--- src/main/java/com/epam/mjc/CollectionService.java
+++ src/main/java/com/epam/mjc/CollectionService.java
@@ -1,27 +1,34 @@
 package com.epam.mjc;
 
+import java.util.Comparator;
 import java.util.List;
 import java.util.Optional;
+import java.util.stream.Collectors;
 
 public class CollectionService {
 
     public List<Integer> filterEvenNumbers(List<Integer> list) {
-        throw new UnsupportedOperationException("You should implement this method.");
+        return list.stream().filter(a -> a % 2 == 0).collect(Collectors.toList());
+//        throw new UnsupportedOperationException("You should implement this method.");
     }
 
     public List<String> toUpperCaseCollection(List<String> list) {
-        throw new UnsupportedOperationException("You should implement this method.");
+        return list.stream().map(v -> v.toUpperCase()).collect(Collectors.toList());
+//        throw new UnsupportedOperationException("You should implement this method.");
     }
 
     public Optional<Integer> findMax(List<Integer> list) {
-        throw new UnsupportedOperationException("You should implement this method.");
+        return list.stream().max(Comparator.comparing(ft -> ft));
+//        throw new UnsupportedOperationException("You should implement this method.");
     }
 
     public Optional<Integer> findMin(List<List<Integer>> list) {
-        throw new UnsupportedOperationException("You should implement this method.");
+        return list.stream().flatMap(List::stream).min(Comparator.comparing(ft -> ft));
+//        throw new UnsupportedOperationException("You should implement this method.");
     }
 
     public Integer sum(List<Integer> list) {
-        throw new UnsupportedOperationException("You should implement this method.");
+        return list.stream().reduce(0, Integer::sum);
+//        throw new UnsupportedOperationException("You should implement this method.");
     }
 }
