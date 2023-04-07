93795e21b0d9d3f8af04a101f687e848572c5f5d
MuhammadJon <mtjohn9411@gmail.com>
2023-04-07 11:09:21 +0500

    Stage 1. Stream API task has been developed

--- src/main/java/com/epam/mjc/CollectionService.java
+++ src/main/java/com/epam/mjc/CollectionService.java
@@ -9,26 +9,21 @@ public class CollectionService {
 
     public List<Integer> filterEvenNumbers(List<Integer> list) {
         return list.stream().filter(a -> a % 2 == 0).collect(Collectors.toList());
-//        throw new UnsupportedOperationException("You should implement this method.");
     }
 
     public List<String> toUpperCaseCollection(List<String> list) {
         return list.stream().map(v -> v.toUpperCase()).collect(Collectors.toList());
-//        throw new UnsupportedOperationException("You should implement this method.");
     }
 
     public Optional<Integer> findMax(List<Integer> list) {
         return list.stream().max(Comparator.comparing(ft -> ft));
-//        throw new UnsupportedOperationException("You should implement this method.");
     }
 
     public Optional<Integer> findMin(List<List<Integer>> list) {
         return list.stream().flatMap(List::stream).min(Comparator.comparing(ft -> ft));
-//        throw new UnsupportedOperationException("You should implement this method.");
     }
 
     public Integer sum(List<Integer> list) {
         return list.stream().reduce(0, Integer::sum);
-//        throw new UnsupportedOperationException("You should implement this method.");
     }
 }
